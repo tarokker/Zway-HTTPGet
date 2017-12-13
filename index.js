99791aa2b3d443bd2bb7b670420b252967655deb
Daniele Maiorana <rasp@fbacreator.com>
2017-12-13 11:32:40 +0100

    nuova versione del modulo con segnalazione udp

--- index.js
+++ index.js
@@ -1,57 +1,23 @@
-/*** HTTPGet Z-Way HA module ****************************************************
-
-Version: 1.0.1
-(c) H Plato, 2017
------------------------------------------------------------------------------
-Author: H Plato <hplato@gmail.com>
-Description:
-   Pushes the status of devices to a URL
-
-   HTTPGet based on https://github.com/goodfield/zway-mqtt
-
- *****************************************************************************/
-
-
-// ----------------------------------------------------------------------------
-// --- Class definition, inheritance and setup
-// ----------------------------------------------------------------------------
-
 function HTTPGet (id, controller) {
     console.log("Initializing HTTPGet module");
     HTTPGet.super_.call(this, id, controller);
 }
 
-//inherits(HTTPGet, BaseModule);
 inherits(HTTPGet, AutomationModule);
-
-
 _module = HTTPGet;
 
-// ----------------------------------------------------------------------------
-// --- Module instance initialized
-// ----------------------------------------------------------------------------
-
 HTTPGet.prototype.init = function (config) {
-    // Call superclass' init (this will process config argument and so on)
-    HTTPGet.super_.prototype.init.call(this, config);
-
-    var self = this;    
-
+	HTTPGet.super_.prototype.init.call(this, config);
+	var self = this;    
 	self.callback = _.bind(self.updateDevice, self);
 	self.controller.devices.on("change:metrics:level", self.callback);
 };
 
 HTTPGet.prototype.stop = function () {
 	var self = this;
-
 	self.controller.devices.off("change:metrics:level", self.callback);
-
-    HTTPGet.super_.prototype.stop.call(this);
+	HTTPGet.super_.prototype.stop.call(this);
 };
-
-// ----------------------------------------------------------------------------
-// --- Module methods
-// ----------------------------------------------------------------------------
 
 
 HTTPGet.prototype.updateDevice = function (device) {
@@ -67,38 +33,24 @@ HTTPGet.prototype.updateDevice = function (device) {
 		}
 	}
 
-	// Maio: verifichiamo se il filtro id matcha
-	if ( typeof(this.config.filter) == 'undefined' || this.config.filter.length == 0 || (this.config.filter + ",").indexOf(device.id + ",") > -1 ) {
-		self.get_url(device, value);
+	// Maio: verifichiamo se il filtro id matcha e cerca nell'array di port e filtri
+	//console.log("HTTPGET: notification id: " + device.id);
+	var filters = ( typeof(this.config.filter) == 'undefined' ? "" : this.config.filter ).split(",");
+	var ports = ( typeof(this.config.ports) == 'undefined' ? "" : this.config.ports ).split(",");
+	if ( filters.length == ports.length ) {
+		var indexObj = filters.indexOf(device.id);
+		if ( indexObj != -1 ) {
+			try {
+	                	var msg = "0001|" + device.id + "|" + value;
+        	        	var port = parseInt(ports[indexObj]);
+				var sock = new sockets.udp();
+				sock.connect("127.0.0.1", port);
+				sock.send(msg);
+				sock.close();
+				console.log("HTTPGET - Sending UDP msg: " + msg + " - on portz: " + port);
+			} catch (e) {
+				console.log("HTTPGET: " + e);
+			}
+		}
 	}
 };
-
-HTTPGet.prototype.get_url = function (device, value) {
-
-	var url = this.config.url
-	url = url.replace("%DEVICE%",device.id);
-	url = url.replace("%VALUE%",value);
-
-	console.log(this.config.url+" : "+device.id+" : "+value+" : "+url);
-
-    var req = {
-        url: url,
-        async: true,
-        success: function(response) {
-            console.log("Request was successful");
-			},
-        error: function(response) {
-            console.log("Can not make request: " + response.statusText); // don't add it to notifications, since it will fill all the notifcations on error
-            } 
-        };
-        // With authorization
-//        if (self.config.user !== undefined && self.config.password !== undefined) {
-//            req.auth = {
-//                    login: self.config.login,
-//                    password: self.config.password
-//            };
-//        }
-    http.request(req);
-};
-
-
